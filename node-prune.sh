52bd03c7637ffcfad538e0edb8aff7439cc1aa19
jeromedecoster <jeromedecoster@users.noreply.github.com>
2024-07-29 11:39:05 +0200

    fix for codebuild

--- node-prune.sh
+++ node-prune.sh
@@ -106,17 +106,25 @@ SIZE_2=$(du -sk node_modules | xargs | cut -f1 -d' ')
 
 
 # compute files removed
-COUNT=$(bc <<< "$COUNT_1 - $COUNT_2")
+COUNT=$(($COUNT_1 - $COUNT_2))
 
 # compute size removed
-SIZE=$(bc -l <<< "($SIZE_1 - $SIZE_2) * 0.001")
+SIZE=$(awk "BEGIN{ print ($SIZE_1 - $SIZE_2) * 0.001 }" | tr ',' '.')
 if [[ $SIZE != 0 ]]; then
-  [[ ${SIZE:0:1} == '.' ]] && SIZE=0${SIZE};
   SIZE=$(awk -v v=$SIZE 'BEGIN{i=index(v,"."); if(i==0){print v}; print substr(v,0,i+2) }')
 fi
 
-echo
-printf "$(tput bold)         files total$(tput sgr0) %i\n" $COUNT_2
-printf "$(tput bold)       files removed$(tput sgr0) %i\n" $COUNT
-printf "$(tput bold)        size removed$(tput sgr0) %s Mb\n" $SIZE
-echo
+# output with style if $TERM is defined
+if [[ -n "$TERM" ]]; then
+  echo
+  printf "$(tput bold)         files total$(tput sgr0) %i\n" $COUNT_2
+  printf "$(tput bold)       files removed$(tput sgr0) %i\n" $COUNT
+  printf "$(tput bold)        size removed$(tput sgr0) %s Mb\n" $SIZE
+  echo
+
+# otherwise simple output (CodeBuild)
+else
+  echo files total: $COUNT_2
+  echo files removed: $COUNT_2
+  echo size removed: $SIZE 
+fi
